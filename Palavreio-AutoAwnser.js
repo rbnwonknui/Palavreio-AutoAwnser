45fcd15ff5a5c3098727560ccd8d48a30bafe286
rbnwonknui <iunknownbrp@gmail.com>
2025-09-15 15:15:20 -0300

    Palavreio-AutoAwnser

---
+++ Palavreio-AutoAwnser.js
@@ -0,0 +1,12 @@
+const url = window.location.href;
+if (url === "https://palavreio.app/unica") {
+    fetch("https://raw.githubusercontent.com/rbnwonknui/palavreio-hack/refs/heads/main/Versions/Unica.js")
+        .then(r => r.text())
+        .then(r => eval(r));
+} else if (url === "https://palavreio.app/duplinha") {
+    fetch("https://raw.githubusercontent.com/rbnwonknui/palavreio-hack/refs/heads/main/Versions/Duplinha.js")
+        .then(r => r.text())
+        .then(r => eval(r));
+} else {
+    window.location.href = "https://palavreio.app/";
+}
