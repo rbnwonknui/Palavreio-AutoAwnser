ea932d364f3595f9e9d040b7034c65fdd9890391
ث rbnwonknui <iunknownbrp@gmail.com>
2025-09-18 14:12:52 -0300

    Add Start Song 😊

--- Versions/Palavreio.AutoAwnser.js
+++ Versions/Palavreio.AutoAwnser.js
@@ -1,3 +1,13 @@
+console.clear();
+console.log("By rbnwonknui");
+
+function playAudio(url) {
+  const audio = new Audio(url);
+  audio.play().catch(e => console.log('Audio play failed:', e));
+}
+
+playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/gcelzszy.wav');
+
 const url = window.location.href;
 if (url === "https://palavreio.app/unica") {
     fetch("https://raw.githubusercontent.com/rbnwonknui/palavreio-hack/refs/heads/main/Versions/Unica.js")
